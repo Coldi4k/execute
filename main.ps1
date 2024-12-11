b4368b8ce8526bd337b2c7267070a5d3c878ef55
Coldi <102551521+Coldi4k@users.noreply.github.com>
2024-12-11 16:55:54 -0600

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,2 @@
-
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show('Hi', 'Greeting', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
